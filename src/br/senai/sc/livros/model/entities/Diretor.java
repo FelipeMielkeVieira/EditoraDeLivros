d0d0be063c6f7a857c9633c41ae03dd1dcfed0ef
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-09-20 20:37:23 -0300

    Delete src/br/senai/sc/livros directory

--- src/br/senai/sc/livros/model/entities/Diretor.java
+++
@@ -1,8 +0,0 @@
-package br.senai.sc.livros.model.entities;
-
-public class Diretor extends Pessoa {
-
-    public Diretor(String nome, String sobrenome, String email, String senha, String cpf, Genero genero) {
-        super(nome, sobrenome, email, senha, cpf, genero);
-    }
-}
